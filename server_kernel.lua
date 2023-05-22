a22152d6952963da5f639513c43d5cfb5f3d03e9
obvKitty <107309229+obvKitty@users.noreply.github.com>
2023-05-22 00:13:58 -0700

    aaaa

--- server_kernel.lua
+++ server_kernel.lua
@@ -1,8 +1,13 @@
 VERSION = "0.1"
 
-function cls(_m)
+function m_cls(_m)
     _m.clear()
     _m.setCursorPos(1,1)
+end
+
+function t_cls(_m)
+    term.clear()
+    term.setCursorPos(1,1)
 end
 
 m = peripheral.wrap("top")
@@ -10,7 +15,8 @@ cls(m)
 m.setTextScale(0.5)
 
 m.setTextColor(colors.pink)
-m.write("KittyOS", VERSION)
+m.write("KittyOS ")
+m.write(VERSION)
 m.setCursorPos(1,3)
 m.setTextColor(colors.white)
 m.write("Status: ")
